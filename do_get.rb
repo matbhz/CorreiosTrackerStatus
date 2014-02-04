d7076e7d427c76c3f3a186e44223692f6be53892
Matheus Felipe <matbhz@MatBook.local>
2014-02-04 21:59:49 -0200

    Parse tracker table

--- do_get.rb
+++ do_get.rb
@@ -1,22 +1,52 @@
 require 'rest-client'
+require 'open-uri'
+require 'nokogiri'
+
 
 class DoGet
 
 
-  POST_URL = 'http://websro.correios.com.br/sro_bin/txect01$.QueryList'
-  TRACKER_NO = 'RB489735409HK'
-  PARAMS_HASH = {:P_ITEMCODE => nil,
-                 :P_LINGUA => 001,
-                 :P_TESTE => nil,
-                 :P_TIPO => 001,
-                 :P_COD_UNI => 'RB489735409HK',
-                 :Z_ACTION => 'Search'}
+  POST_URL = "http://websro.correios.com.br/sro_bin/txect01$.QueryList"
+  TRACKER_NO = "RB489735409HK"
+  PARAMS = "P_ITEMCODE=&P_LINGUA=001&P_TESTE=&P_TIPO=001&P_COD_UNI=#{TRACKER_NO}&Z_ACTION=Search"
+
+  LOCATION_COLUMNS = 3
+
+  response = RestClient.post POST_URL, PARAMS
+
+  details_table = Nokogiri::HTML(response).xpath("//table")
+
+  json_response = {}
+
+  details_table.xpath("//following::tr[2]").each do |row|
+
+    columns = row.children
+    if columns.size == LOCATION_COLUMNS
 
 
-  response = RestClient.post POST_URL, PARAMS_HASH
+      date = columns[0].text
+      location = columns[1].text
+      situation = columns[2].text
 
 
-  p response
+      p "Data: #{date} | Local: #{location} | Situação: #{situation}"
 
+    else
+      details = columns[0].text
+
+      p "Detalhe: #{details}"
+
+    end
+
+  end
+
+
+=begin
+ 0 - Headers Data/Local/Situação
+ 1..x - Cada atualização + Situação
+    0 - Local
+    1 - detalhes
+
+=end
 
 end
---
+++ websro.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'websro'
+  s.version     = '0.1'
+  s.date        = '2014-02-04'
+  s.summary     = "Correios Websro RESTful service"
+  s.description = "A simple brazilian Correios Websro RESTful (mail) service to get the status of your package :)"
+  s.authors     = ["Matheus Felipe"]
+  s.email       = 'matbhz@gmail.com'
+  s.files       = ["do_get.rb"]
+  s.homepage    =
+      'http://rubygems.org/gems/hola'
+  s.license       = 'matbhz'
+end
