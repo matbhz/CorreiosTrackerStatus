8673d7f06f12ccfd28c2e295e29acceeb935b6f2
Matheus Felipe <matbhz@MatBook.local>
2014-02-04 23:25:14 -0200

    Add Module Correios and method to be called with custom tracker_no

---
+++ Correios.rb
@@ -0,0 +1,33 @@
+require 'rest-client'
+require 'nokogiri'
+
+module Correios
+
+  POST_URL = "http://websro.correios.com.br/sro_bin/txect01$.QueryList"
+  PARAMS = "P_ITEMCODE=&P_LINGUA=001&P_TESTE=&P_TIPO=001&P_COD_UNI=%%tracker_no%%&Z_ACTION=Search"
+
+  def self.get tracker_no
+
+    PARAMS["%%tracker_no%%"] = tracker_no
+    response = RestClient.post POST_URL, PARAMS
+    details_table = Nokogiri::HTML(response).xpath("//table//following::tr[2]")
+
+    json_response = {:tracker_no => tracker_no, :info => []}
+
+    details_table.each do |row|
+      info = {}
+      columns = row.children
+      if columns.size == 3
+        date = columns[0].text
+        location = columns[1].text
+        situation = columns[2].text
+        info = {:date => date, :location => location, :situation => situation}
+        json_response[:info].push info
+      else
+        details = columns[0].text
+        info[:details] = details
+      end
+    end
+    json_response.to_json
+  end
+end
